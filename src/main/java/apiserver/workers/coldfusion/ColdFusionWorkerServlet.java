0debc81ba182b4fce26c08b0f76f25e3b5b9b27b
Mike Nimer <mikenimer@yahoo.com>
2014-10-08 01:21:51 -0500

    add missing files

--- src/main/java/apiserver/workers/coldfusion/ColdFusionWorkerServlet.java
+++ src/main/java/apiserver/workers/coldfusion/ColdFusionWorkerServlet.java
@@ -4,6 +4,8 @@ import org.gridgain.grid.Grid;
 import org.gridgain.grid.GridConfiguration;
 import org.gridgain.grid.GridGain;
 import org.gridgain.grid.marshaller.optimized.GridOptimizedMarshaller;
+import org.gridgain.grid.spi.discovery.tcp.GridTcpDiscoverySpi;
+import org.gridgain.grid.spi.discovery.tcp.ipfinder.multicast.GridTcpDiscoveryMulticastIpFinder;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -27,7 +29,7 @@ public class ColdFusionWorkerServlet implements Servlet
     public void init(ServletConfig config) throws ServletException {
 
         System.out.println("***************************************");
-        System.out.println("ApiServer Grid Manager");
+        System.out.println("ApiAppEngine Grid Manager");
         System.out.println("***************************************");
 
         try
@@ -72,18 +74,35 @@ public class ColdFusionWorkerServlet implements Servlet
 
     private GridConfiguration getGridConfiguration() {
         Map<String, String> userAttr = new HashMap<String, String>();
-        userAttr.put("ROLE", "coldfusion-worker");
+        userAttr.put("ROLE", "ApiAppEngine");
+        userAttr.put("ROLE", "connector-coldfusion");
 
-        GridOptimizedMarshaller marshaller = new GridOptimizedMarshaller();
-        marshaller.setRequireSerializable(false);
+        GridOptimizedMarshaller gom = new GridOptimizedMarshaller();
+        gom.setRequireSerializable(false);
+
+        GridTcpDiscoveryMulticastIpFinder fndr = new GridTcpDiscoveryMulticastIpFinder();
+        fndr.setLocalAddress("127.0.0.1");
+        GridTcpDiscoverySpi spi = new GridTcpDiscoverySpi();
+        spi.setIpFinder(fndr);
 
         GridConfiguration gc = new GridConfiguration();
-        gc.setGridName("ApiServer");
-        gc.setPeerClassLoadingEnabled(false);
-        gc.setRestEnabled(false);
+        gc.setGridName("ApiAppEngine");
+        gc.setPeerClassLoadingEnabled(true);
         gc.setUserAttributes(userAttr);
-        gc.setMarshaller(marshaller);
+        gc.setMarshaller(gom);
+        gc.setDiscoverySpi(spi);
 
+
+
+        /**
+         * Configure grid to use multicast based discovery.
+        GridTcpDiscoveryMulticastIpFinder fndr = new GridTcpDiscoveryMulticastIpFinder();
+        GridTcpDiscoverySpi gd = new GridTcpDiscoverySpi();
+        gd.setLocalAddress("127.0.0.1");
+        gd.setIpFinder(fndr);
+        //fndr.setMulticastGroup("127.0.0.1");
+        gc.setDiscoverySpi(gd);
+         */
 
         //GridCacheConfiguration gcc = new GridCacheConfiguration();
         //gcc.setCacheMode(GridCacheMode.PARTITIONED);
